c388a38c30087920b38f33fd57dfe56fa410214c
Saowaluck Suksawat <poppy@odds.team>
2023-01-28 16:31:23 +0700

    ♻️ Extract state to props

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,24 @@
 import BookList from "../components/BookList";
 
+const books = [
+  {
+    title: "Domain Driven Design",
+    converImg:
+      "https://m.media-amazon.com/images/I/61aFldsgAmL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    finished: true,
+  },
+  {
+    title: "Clean Code",
+    converImg:
+      "https://m.media-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    finished: false,
+  },
+]
+
 const HomePage = () => {
   return (
     <>
-      <BookList />
+      <BookList books={books}/>
     </>
   );
 };
--- src/components/BookList/index.jsx
+++ src/components/BookList/index.jsx
@@ -1,22 +1,8 @@
 "use client"
 
 // 💡 book contains title: string, coverImg: string, finished: boolean
-const books = [
-  {
-    title: "Domain Driven Design",
-    converImg:
-      "https://m.media-amazon.com/images/I/61aFldsgAmL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
-    finished: true,
-  },
-  {
-    title: "Clean Code",
-    converImg:
-      "https://m.media-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-    finished: false,
-  },
-]
 
-const BookList = () => {
+const BookList = ({books}) => {
   return (
     <div className="bookList">
       {books.map((book) => (
