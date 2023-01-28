3895dd6bbf8ee9855ce550d92e9fbe9c9bc55812
pop <popsoften@gmail.com>
2023-01-28 09:16:41 +0000

    ✅ Done excersise

--- src/components/BookList/index.jsx
+++ src/components/BookList/index.jsx
@@ -1,29 +1,37 @@
 "use client";
 
 // 💡 book contains title: string, coverImg: string, finished: boolean
+const books = [
+  {
+    title: "Domain Driven Design",
+    converImg:
+      "https://m.media-amazon.com/images/I/61aFldsgAmL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    finished: true,
+  },
+  {
+    title: "Clean Code",
+    converImg:
+      "https://m.media-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    finished: true,
+  },
+];
 
 const BookList = () => {
   return (
     <div className="bookList">
-      <div className="book">
-        <div className="book-thumbnail">
-          <img
-            src="https://m.media-amazon.com/images/I/61aFldsgAmL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
-            alt="book cover"
-          />
-        </div>
-        <p className="text-title">Domain Driven Design</p>
-        <span className="readingStatus">Finish</span>
-      </div>
-      <div className="book">
-        <div className="book-thumbnail">
-          <img
-            src="https://m.media-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-            alt="book cover"
-          />
-        </div>
-        <p className="text-title">Clean Code</p>
-      </div>
+      {books.map((book) => {
+        <>
+          <div className="book">
+            <div className="book-thumbnail">
+              <img src={book.converImg} alt="book cover" />
+            </div>
+            <p className="text-title">{book.title}</p>
+            <span className="readingStatus">
+              {book.finished ? "finished" : ""}
+            </span>
+          </div>
+        </>;
+      })}
     </div>
   );
 };
