502675c358c6f71c0b4ce3f905ed69bd3685c70b
pop <popsoften@gmail.com>
2023-01-28 08:52:56 +0000

    ✅ done excerise

--- src/components/Quiz4.jsx
+++ src/components/Quiz4.jsx
@@ -1,1 +1,25 @@
 // Write component name Quiz4 and display your name
+const book = {
+  text: "Clean Code",
+  price: 10,
+};
+const Quiz4 = () => {
+  return (
+    <>
+      Quiz 4<Child book={book} />
+    </>
+  );
+};
+
+//destructuring
+const Child = ({ book }) => {
+  return (
+    <ul>
+      <li>Book: {book.text}</li>
+      <li>Price: ${book.price}</li>
+      <li>Include VAT ${book.price + 1}</li>
+    </ul>
+  );
+};
+
+export default Quiz4;
